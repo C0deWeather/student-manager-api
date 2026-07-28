0f101a4ebad1397eb08f3654f7fb902f46367c93
Bright Frank <frankbright211@gmail.com>
2026-07-28 22:30:31 +0100

    Implement data CRUD functions

---
+++ data.js
@@ -0,0 +1,43 @@
+import { NotFoundError, ValidationError } from './errors.js';
+
+const STUDENTS = [];
+let nextId = 1;
+
+export function addStudent(student) {
+  const newStudent = {
+    id: nextId++,
+    ...student
+  };
+  STUDENTS.push(newStudent);
+  return newStudent;
+}
+
+export function getAllStudents() {
+  return structuredClone(STUDENTS);
+}
+
+export function getStudentById(id) {
+  const student = STUDENTS.find(student => student.id === id);
+  if (!student) {
+    throw new NotFoundError("student not found");
+  }
+  return student;
+}
+
+export function updateStudent(id, student) {
+  const currentStudent = getStudentById(id);
+  
+  currentStudent.fullName = student.fullName;
+  currentStudent.gender = student.gender;
+  currentStaudent.department = student.department;
+  currentStudent.level = student.level;
+  return currentStudent;
+}
+
+export function deleteStudent(id) {
+  const indexInArray = STUDENTS.findIndex(student => student.id === id);
+  if (indexInArray === -1) {
+    throw new NotFoundError("student not found");
+  }
+  tasks.splice(indexInArray, 1);
+}
